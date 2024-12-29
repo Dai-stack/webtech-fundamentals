1ee7d45cacf0d2ec62a02263c10658f816623f53
nishi <nishi@NISHI_D>
2024-12-29 16:35:22 +0900

    2024年12月29日作業分

--- chapter06/simple-webserver2/main.go
+++ chapter06/simple-webserver2/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("static"))) // <1>
+	http.Handle("/", http.FileServer(http.Dir("static"))) // <1> staticが起点ディレクトリになり、そこからファイルを探す
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("failed to start : ", err)
--- chapter06/tinytodo-01-base/main.go
+++ chapter06/tinytodo-01-base/main.go
@@ -9,16 +9,20 @@ import (
 var todoList []string // <1>
 
 func handleTodo(w http.ResponseWriter, r *http.Request) { // <5>
-	t, _ := template.ParseFiles("templates/todo.html") // <6>
-	t.Execute(w, todoList)                             // <7>
+	t, _ := template.ParseFiles("templates/todo.html") // <6> テンプレートを解析し、変数にいれる
+	// テンプレートと変数toDoListを組み合わせ動的にHTMLを生成し、レスポンスとして返している。
+	t.Execute(w, todoList)
 }
 
 func main() {
 	todoList = append(todoList, "顔を洗う", "朝食を食べる", "歯を磨く") // <2>
 
-	http.Handle("/static/",
+	// １つ目のstatic→クライアントがアクセ　するURLパス　ファイルシステム上で静的ファイルを探すための基点
+	// 2つ目のstatic→サーバー側のファイルが存在するディレクトリ名
+	http.Handle("/static/", //
 		http.StripPrefix("/static/", http.FileServer(http.Dir("static")))) // <3>
 
+	//　パスtodoでアクセスがきた場合、関数handleTodoを返す
 	http.HandleFunc("/todo", handleTodo) // <4>
 
 	err := http.ListenAndServe(":8080", nil)
